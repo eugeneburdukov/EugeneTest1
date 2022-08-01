2d07199b2107309312cbd5f4d9b2f24f80dcb39b
eugeneburdukov <eugenebkv@yahoo.com.ua>
2022-08-01 16:39:14 +0300

    Initial Commit

---
+++ EugeneTest1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EugeneTest1
+//
+//  Created by Burdukov Evgeniy on 8/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
